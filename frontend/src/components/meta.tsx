f84924c2a1f1dc5f8e697099df87f28a37f9c0c0
akitomonam <akitomonam@gmail.com>
2023-11-16 14:27:44 +0900

    Update title and description in Meta component

--- frontend/src/components/meta.tsx
+++ frontend/src/components/meta.tsx
@@ -1,21 +1,13 @@
 import { buildUrl } from "@/utils/buildUrl";
 import Head from "next/head";
 export const Meta = () => {
-  const title = "ChatVRM";
+  const title = "AI Shimazaki";
   const description =
-    "Webブラウザだけで3Dキャラクターとの会話を、マイクやテキスト入力、音声合成を用いて楽しめます。キャラクター（VRM）の変更や性格設定、音声調整もできます。";
-  const imageUrl = "https://pixiv.github.io/ChatVRM/ogp.png";
+  "電気通信大学 技術職員である島崎をAI化したものです 雑談や電気通信大学に関する質問ができます";
   return (
     <Head>
       <title>{title}</title>
       <meta name="description" content={description} />
-      <meta property="og:title" content={title} />
-      <meta property="og:description" content={description} />
-      <meta property="og:image" content={imageUrl} />
-      <meta name="twitter:card" content="summary_large_image" />
-      <meta name="twitter:title" content={title} />
-      <meta name="twitter:description" content={description} />
-      <meta name="twitter:image" content={imageUrl} />
     </Head>
   );
 };
