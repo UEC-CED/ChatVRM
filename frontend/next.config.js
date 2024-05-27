baf0a8927a33646d9a287cb9fabf1a43c96c87b3
hyoutan0726 <hyoutan0726@gmail.com>
2024-05-27 13:40:01 +0900

    fix: npm run devがホットリロードに対応していない問題の解決

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,16 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // assetPrefix: process.env.BASE_PATH || "",
-  // basePath: process.env.BASE_PATH || "",
   assetPrefix: '/ced-iot',
   basePath: '/ced-iot', 
   trailingSlash: true,
   publicRuntimeConfig: {
-    // root: process.env.BASE_PATH || "",
     root: '/ced-iot'
   },
   optimizeFonts: false,
+  webpackDevMiddleware: config => {
+    config.watchOptions = {
+      poll: 1000,   // 毎秒1回ファイルの変更を確認
+      aggregateTimeout: 300, // 300ms待機して変更を確認
+    };
+    return config;
+  },
 };
 
 module.exports = nextConfig;
